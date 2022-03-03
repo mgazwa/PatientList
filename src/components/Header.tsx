10b6af5b540273ded86e1a2e1ad3149a07a2ffdc
mgazwa <mgazwa@gmail.com>
2022-03-03 22:01:56 +0100

    Form creaated

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {AppBar, Container, Toolbar, Typography} from "@material-ui/core";
+
+
+export const Header = () => {
+    return(
+        <AppBar position='static'>
+            <Toolbar>
+                <Container maxWidth={'md'}>
+                    <Typography variant={'h6'}>
+                        Gabinet Lekarski
+                    </Typography>
+                </Container>
+            </Toolbar>
+        </AppBar>
+    )
+}
